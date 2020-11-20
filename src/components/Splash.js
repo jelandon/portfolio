a9fb788989a17f7072cd9a2d3956e5519780649b
jelandon <jelandon@gmail.com>
2020-11-19 16:07:21 -0800

    Add splash to homescreen

---
+++ src/components/Splash.js
@@ -0,0 +1,24 @@
+function Splash() {
+  return (
+	<main>
+		<div class="splash">
+			<div class="left-splash">
+				Hi there! My name is John and I am a
+			</div>
+			<div class="right-splash">
+				<div class="strikethrough">
+					<s>Front-End Developer</s><br />
+					<s>Visual Designer</s><br />
+					<s>Product Architect</s><br />
+					<s>Content Strategist</s><br />
+				</div>
+				<div class="answer">
+					PROBLEM SOLVER
+				</div>
+			</div>
+		</div>
+	</main>
+  );
+}
+
+export default Splash;
