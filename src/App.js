1352da503ba755954f62432bbe78ad1e76c7c983
jelandon <jelandon@gmail.com>
2020-11-19 16:12:58 -0800

    Add Splash.js to homescreen

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Header from './components/Header.js';
+import Splash from './components/Splash.js';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Splash />
     </div>
   );
 }
